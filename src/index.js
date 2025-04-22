f7975b0a69d8519c8ee040947c56f62c741a7bee
hoafn0730 <hoantran0730@gmail.com>
2025-04-22 11:47:16 +0700

    update

--- src/index.js
+++ src/index.js
@@ -25,7 +25,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: ['http://localhost:3000', 'http://localhost:5173'],
+        origin: ['http://localhost:3000', 'http://localhost:5173', 'https://fdemy.id.vn'],
         methods: ['GET', 'POST'],
         // allowedHeaders: ['my-custom-header'],
         credentials: true,
